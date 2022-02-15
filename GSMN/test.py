cac9a627d428881f4f0d407baf4ce1aac00d023a
F.Wang <59386888+WangFei-2019@users.noreply.github.com>
2022-02-15 16:39:48 +0800

    Delete GSMN directory

--- GSMN/test.py
+++
@@ -1,8 +0,0 @@
-from vocab import Vocabulary
-import evaluation
-import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-RUN_PATH = "models1/model_best.pth.tar"
-DATA_PATH = "/media/ubuntu/data/chunxiao/"
-evaluation.evalrank(RUN_PATH, data_path=DATA_PATH, split="test")
--- GSMN/testall.py
+++
@@ -1,8 +0,0 @@
-from vocab import Vocabulary
-import evaluation
-import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-RUN_PATH = "model_best.pth.tar"
-DATA_PATH = "/media/ubuntu/data/chunxiao/"
-evaluation.evalrank(RUN_PATH, data_path=DATA_PATH, split="testall",fold5=True)
